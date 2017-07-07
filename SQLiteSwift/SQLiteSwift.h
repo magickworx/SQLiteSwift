30b37e4510e7c468d128c98f49bcf4066e85d16a
MagickWorX <kouichi.abe.wall@magickworx.com>
2017-07-07 14:51:12 +0900

    Initial Commit

---
+++ SQLiteSwift/SQLiteSwift.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteSwift.h
+//  SQLiteSwift
+//
+//  Created by Kouichi ABE (WALL) on 2017/07/07.
+//  Copyright © 2017年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLiteSwift.
+FOUNDATION_EXPORT double SQLiteSwiftVersionNumber;
+
+//! Project version string for SQLiteSwift.
+FOUNDATION_EXPORT const unsigned char SQLiteSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteSwift/PublicHeader.h>
+
+
